9b47981658f0858145d749e166be5d6c77497934
Pranjali Thakur <pranjali.thakur@circle.com>
2024-10-02 14:56:31 -0400

    Add clippy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 10;
+    let y = 20; // Clippy will warn about `y` being unused
+    let sum = x + 5;
+
+    let value: Option<i32> = None;
+        
+    // Clippy will warn about using `unwrap()` on an `Option` that can be `None`.
+    let z = value.unwrap();  // This will panic if `value` is `None`
+    
+
+    println!("The sum is: {}", sum);
+    println!("{}", z);
+
 }
