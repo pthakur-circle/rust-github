029e0f3527e55041d05055f928bbb76618a2648d
Pranjali Thakur <pranjali.thakur@circle.com>
2024-10-03 13:04:39 -0400

    cargo tarpaulin

--- src/main.rs
+++ src/main.rs
@@ -13,3 +13,25 @@ fn main() {
     println!("{}", z);
 
 }
+
+// Tarpaulin
+/// A simple function that adds two numbers.
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+        assert_eq!(add(-1, 1), 0);
+    }
+
+    // #[test]
+    // fn test_failure() {
+    //     assert_eq!(add(1, 1), 3); // This test will fail
+    // }
+}
